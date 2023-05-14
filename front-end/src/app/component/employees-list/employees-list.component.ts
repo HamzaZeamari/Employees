af8c8437ca69f5f22f713e09ff1efa2c2a71933b
Hamza Zeamari <zeam.hamza@gmail.com>
2023-05-14 14:15:16 +0200

    Update

--- front-end/src/app/component/employees-list/employees-list.component.ts
+++ front-end/src/app/component/employees-list/employees-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Employee} from "../../models/employee.model";
 
 @Component({
   selector: 'app-employees-list',
@@ -7,4 +8,48 @@ import { Component } from '@angular/core';
 })
 export class EmployeesListComponent {
 
+  employees : Employee[] = [
+    {
+      id: '1',
+      name: 'Hamza',
+      email: 'h@gmail.com',
+      phone: 123,
+      salary: 1111,
+      department:'IT',
+    },
+    {
+      id: '2',
+      name: 'Hamza',
+      email: 'h@gmail.com',
+      phone: 123,
+      salary: 1111,
+      department:'IT',
+    },
+    {
+      id: '3',
+      name: 'Hamza',
+      email: 'h@gmail.com',
+      phone: 123,
+      salary: 1111,
+      department:'IT',
+    },
+    {
+      id: '4',
+      name: 'Hamza',
+      email: 'h@gmail.com',
+      phone: 123,
+      salary: 1111,
+      department:'IT',
+    },
+  ];
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    this.employees.push();
+  }
+
+  protected readonly length = length;
+  protected readonly length = length;
 }
